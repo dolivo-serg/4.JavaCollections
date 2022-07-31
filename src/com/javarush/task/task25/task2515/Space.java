0e0c230d0de49b5375bd84e125e7b938b247b5af
Sehii <dolivo.serg@gmail.com>
2022-07-31 20:42:20 +0300

    task2515

--- src/com/javarush/task/task25/task2515/Space.java
+++ src/com/javarush/task/task25/task2515/Space.java
@@ -1,6 +1,73 @@
 package com.javarush.task.task25.task2515;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Space {
+    private int width;
+    private int height;
+    private SpaceShip ship;
+    private List<Ufo> ufos = new ArrayList<>();
+    private List<Rocket> rockets = new ArrayList<>();
+    private List<Bomb> bombs = new ArrayList<>();
+
+    public Space(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public SpaceShip getShip() {
+        return ship;
+    }
+
+    public void setShip(SpaceShip ship) {
+        this.ship = ship;
+    }
+
+    public List<Ufo> getUfos() {
+        return ufos;
+    }
+
+    public void setUfos(List<Ufo> ufos) {
+        this.ufos = ufos;
+    }
+
+    public List<Rocket> getRockets() {
+        return rockets;
+    }
+
+    public void setRockets(List<Rocket> rockets) {
+        this.rockets = rockets;
+    }
+
+    public List<Bomb> getBombs() {
+        return bombs;
+    }
+
+    public void setBombs(List<Bomb> bombs) {
+        this.bombs = bombs;
+    }
+
+    public static void run(){}
+    public static void draw(){}
+    public static void sleep(int ms){}
+
     public static void main(String[] args) {
 
     }
