6bb3b036257e38bc6572c7e66b4ed3374b9b1453
Sehii <dolivo.serg@gmail.com>
2022-08-12 18:41:28 +0300

    task3505, task3506

---
+++ src/com/javarush/task/task35/task3505/ConvertableUtil.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task35.task3505;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ConvertableUtil {
+    public static <K, V extends Convertable<K>> Map<K, V> convert(List<V> list) {
+        Map<K, V> result = new HashMap<>();
+        list.forEach(e -> result.put(e.getKey(), e));
+        return result;
+    }
+
+}
