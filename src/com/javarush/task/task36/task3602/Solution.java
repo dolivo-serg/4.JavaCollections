4875309bf1b313e459fea3e0e709f13e706c56e8
Sehii <dolivo.serg@gmail.com>
2022-08-17 22:28:48 +0300

    tack3602, tack3603

---
+++ src/com/javarush/task/task36/task3602/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task36.task3602;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/* 
+Найти класс по описанию Ӏ Java Collections: 6 уровень, 6 лекция
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getExpectedClass());
+    }
+
+    public static Class getExpectedClass() {
+        return Arrays.stream(Collections.class.getDeclaredClasses())
+                .filter(clazz -> clazz.getSimpleName().equals("EmptyList"))
+                .findAny()
+                .get();
+    }
+}
