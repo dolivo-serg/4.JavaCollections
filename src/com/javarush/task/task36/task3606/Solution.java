cbe314b9ddb0b3e307ac8e206f80b2f2e319ca97
Sehii <dolivo.serg@gmail.com>
2022-08-21 21:14:09 +0300

    task3606 ClassLoader и Reflection

---
+++ src/com/javarush/task/task36/task3606/Solution.java
@@ -0,0 +1,57 @@
+package com.javarush.task.task36.task3606;
+
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Осваиваем ClassLoader и Reflection
+*/
+
+public class Solution {
+    private List<Class> hiddenClasses = new ArrayList<>();
+    private String packageName;
+
+    public Solution(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Solution solution = new Solution(Solution.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "com/javarush/task/task36/task3606/data/second");
+        solution.scanFileSystem();
+        System.out.println(solution.getHiddenClassObjectByKey("secondhiddenclassimpl"));
+        System.out.println(solution.getHiddenClassObjectByKey("firsthiddenclassimpl"));
+        System.out.println(solution.getHiddenClassObjectByKey("packa"));
+    }
+
+    public void scanFileSystem() throws ClassNotFoundException {
+        File dir = new File(packageName);
+        ClassLoader classLoader = Solution.class.getClassLoader();
+        for (String fileName : dir.list()) {
+            if (fileName.endsWith(".class")) {
+                String className = packageName
+                        .replaceAll("[/\\\\]", ".")
+                        .substring(packageName.lastIndexOf("ru/")) + "." + fileName.substring(0, fileName.length() - 6);
+                Class<?> clazz = classLoader.loadClass(className);
+                hiddenClasses.add(clazz);
+            }
+        }
+    }
+
+    public HiddenClass getHiddenClassObjectByKey(String key) {
+        Class<?> clazz = hiddenClasses.stream()
+                .filter(c -> c.getSimpleName().toLowerCase().startsWith(key.toLowerCase()))
+                .findFirst()
+                .get();
+        try {
+        Constructor<?> constructor = clazz.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        return (HiddenClass) constructor.newInstance();
+        } catch (NoSuchMethodException | InstantiationException | InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
