6852398db3c8da033d09d6959c2e0536aedeed5f
Sehii <dolivo.serg@gmail.com>
2022-08-24 22:59:26 +0300

    task3310 Shortener

--- src/com/javarush/task/task33/task3310/Solution.java
+++ src/com/javarush/task/task33/task3310/Solution.java
@@ -1,7 +1,52 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.HashMapStorageStrategy;
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Solution {
     public static void main(String[] args) {
+        StorageStrategy strategy = new HashMapStorageStrategy();
+        testStrategy(strategy, 10_000L);
+    }
 
+    public static Set<Long> getIds(Shortener shortener, Set<String> strings) {
+        return strings.stream()
+                .map(shortener::getId)
+                .collect(Collectors.toSet());
+    }
+
+    public static Set<String> getStrings(Shortener shortener, Set<Long> keys) {
+        return keys.stream()
+                .map(shortener::getString)
+                .collect(Collectors.toSet());
+    }
+
+    public static void testStrategy(StorageStrategy strategy, long elementsNumber) {
+        String strategyName = strategy.getClass().getSimpleName();
+        System.out.println(strategyName);
+
+        Set<String> testSet = new HashSet<>();
+        for (int i = 0; i < elementsNumber; i++) {
+            testSet.add(Helper.generateRandomString());
+        }
+
+        Shortener shortener = new Shortener(strategy);
+
+        long timeStart = new Date().getTime();
+        Set<Long> testSetIDs = getIds(shortener, testSet);
+        long timeEnd = new Date().getTime();
+        System.out.println("Test getIds: " + (timeEnd - timeStart) + "ms");
+
+        timeStart = new Date().getTime();
+        Set<String> resultSet = getStrings(shortener, testSetIDs);
+        timeEnd = new Date().getTime();
+        System.out.println("Test getStrings: " + (timeEnd - timeStart) + "ms");
+
+        System.out.println(testSet.equals(resultSet) ? "Тест пройден." : "Тест не пройден.");
     }
 }
---
+++ src/com/javarush/task/task33/task3310/strategy/OurHashMapStorageStrategy.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task33.task3310.strategy;
+
+public class OurHashMapStorageStrategy implements StorageStrategy {
+
+    static final int DEFAULT_INITIAL_CAPACITY = 16;
+    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
+    Entry[] table = new Entry[DEFAULT_INITIAL_CAPACITY];
+    int size;
+    int threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
+    float loadFactor = DEFAULT_LOAD_FACTOR;
+
+
+    @Override
+    public boolean containsKey(Long key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(String value) {
+        return false;
+    }
+
+    @Override
+    public void put(Long key, String value) {
+
+    }
+
+    @Override
+    public Long getKey(String value) {
+        return null;
+    }
+
+    @Override
+    public String getValue(Long key) {
+        return null;
+    }
+}
