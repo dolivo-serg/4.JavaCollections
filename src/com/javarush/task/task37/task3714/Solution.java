ec06cbf0343e035cfda6fa2f50a1556b603aa504
Sehii <dolivo.serg@gmail.com>
2022-09-04 18:54:49 +0300

    task3714 romanToInteger

---
+++ src/com/javarush/task/task37/task3714/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task37.task3714;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Древний Рим
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input a roman number to be converted to decimal: ");
+        String romanString = bufferedReader.readLine();
+        System.out.println("Conversion result equals " + romanToInteger(romanString));
+    }
+
+    public static int romanToInteger(String s) {
+        Map<Character, Integer> romanMap = new HashMap<Character, Integer>(){{
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
+        }};
+
+    int result = romanMap.get(s.charAt(s.length() - 1));
+        for (int i = s.length() - 2; i >= 0; --i) {
+            if (romanMap.get(s.charAt(i)) < romanMap.get(s.charAt(i + 1))) {
+                result -= romanMap.get(s.charAt(i));
+            } else {
+                result += romanMap.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
+}
