ac8b6682a571b91ca40bb6cbee7f41b5a013ce51
Sehii <dolivo.serg@gmail.com>
2022-09-06 22:06:35 +0300

    task3709 SecurityProxyConnector

--- src/com/javarush/task/task37/task3709/connectors/SecurityProxyConnector.java
+++ src/com/javarush/task/task37/task3709/connectors/SecurityProxyConnector.java
@@ -3,17 +3,21 @@ package com.javarush.task.task37.task3709.connectors;
 import com.javarush.task.task37.task3709.security.SecurityChecker;
 import com.javarush.task.task37.task3709.security.SecurityCheckerImpl;
 
-public class SecurityProxyConnector implements Connector{
-    private SecurityChecker securityChecker;
-    private SimpleConnector simpleConnector;
-
-    public SecurityProxyConnector(String resourceString) {
-        this.simpleConnector = new SimpleConnector(resourceString);
-        this.securityChecker = new SecurityCheckerImpl();
-    }
+public class SecurityProxyConnector implements Connector {
+    SimpleConnector simpleConnector;
+    SecurityChecker securityChecker = new SecurityCheckerImpl();
 
     @Override
     public void connect() {
+        System.out.println("Performing security check...");
+        if (securityChecker.performSecurityCheck()) {
+            simpleConnector.connect();
+        } else {
+            System.out.println("FAILED SECURITY CHECK, WON'T CONNECT!");
+        }
+    }
 
+    public SecurityProxyConnector(String resourceString) {
+        this.simpleConnector = new SimpleConnector(resourceString);
     }
 }
