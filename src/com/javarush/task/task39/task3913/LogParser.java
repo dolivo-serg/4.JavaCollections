c972a8078696cbb63479d3390a5abf7b4d506442
Sehii <dolivo.serg@gmail.com>
2022-09-15 21:58:18 +0300

    task3913

--- src/com/javarush/task/task39/task3913/LogParser.java
+++ src/com/javarush/task/task39/task3913/LogParser.java
@@ -2,16 +2,87 @@ package com.javarush.task.task39.task3913;
 
 import com.javarush.task.task39.task3913.query.IPQuery;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
-public class LogParser {
+public class LogParser implements IPQuery {
+    private Path logDir;
+    private List<LogEntity> logEntities = new ArrayList<>();
+    private DateFormat simpleDateFormat = new SimpleDateFormat("d.M.yyyy H:m:s");
+
+    public LogParser(Path logDir) {
+        this.logDir = logDir;
+    }
+
+    @Override
+    public int getNumberOfUniqueIPs(Date after, Date before) {
+        return 0;
+    }
+
+    @Override
+    public Set<String> getUniqueIPs(Date after, Date before) {
+        return null;
+    }
+
+    @Override
+    public Set<String> getIPsForUser(String user, Date after, Date before) {
+        return null;
+    }
+
+    @Override
+    public Set<String> getIPsForEvent(Event event, Date after, Date before) {
+        return null;
+    }
+
+    @Override
+    public Set<String> getIPsForStatus(Status status, Date after, Date before) {
+        return null;
+    }
+
+    private class LogEntity {
+        private String ip;
+        private String user;
+        private Date date;
+        private Event event;
+        private int eventAdditionalParameter;
+        private Status status;
+
+        public LogEntity(String ip, String user, Date date, Event event, int eventAdditionalParameter, Status status) {
+            this.ip = ip;
+            this.user = user;
+            this.date = date;
+            this.event = event;
+            this.eventAdditionalParameter = eventAdditionalParameter;
+            this.status = status;
+        }
+
+        public String getIp() {
+            return ip;
+        }
+
+        public String getUser() {
+            return user;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public Event getEvent() {
+            return event;
+        }
+
+        public int getEventAdditionalParameter() {
+            return eventAdditionalParameter;
+        }
+
+        public Status getStatus() {
+            return status;
+        }
+    }
 }
