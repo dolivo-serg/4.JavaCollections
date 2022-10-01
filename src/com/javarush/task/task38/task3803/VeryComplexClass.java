73af32929346cee3273d75bff4ff863efcafbd57
Sehii <dolivo.serg@gmail.com>
2022-10-01 13:24:09 +0300

    task3803

--- src/com/javarush/task/task38/task3803/VeryComplexClass.java
+++ src/com/javarush/task/task38/task3803/VeryComplexClass.java
@@ -1,20 +1,18 @@
 package com.javarush.task.task38.task3803;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
-/* 
+/*
 Runtime исключения (unchecked exception)
 */
 
 public class VeryComplexClass {
     public void methodThrowsClassCastException() {
-
+        Object obj = new VeryComplexClass();
+        Integer integer = (Integer) obj;
     }
 
     public void methodThrowsNullPointerException() {
-
+        String string = null;
+        string.toLowerCase();
     }
 
     public static void main(String[] args) {
