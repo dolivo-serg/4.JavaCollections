69bafab49d1b611e1b5ef2506bad155a6b962fbd
Sehii <dolivo.serg@gmail.com>
2022-10-09 17:01:22 +0300

    task2810 Aggregator

--- src/com/javarush/task/task28/task2810/Controller.java
+++ src/com/javarush/task/task28/task2810/Controller.java
@@ -1,33 +1,19 @@
 package com.javarush.task.task28.task2810;
 
-import com.javarush.task.task28.task2810.model.Provider;
-import com.javarush.task.task28.task2810.vo.Vacancy;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.javarush.task.task28.task2810.model.Model;
 
 public class Controller {
 
-    private Provider[] providers;
+    private Model model;
 
-    public Controller(Provider... providers) {
-        if (providers.length == 0) throw new IllegalArgumentException();
-        this.providers = providers;
-    }
-
-    @Override
-    public String toString() {
-        return "Controller{" +
-                "providers=" + Arrays.toString(providers) +
-                '}';
-    }
-
-    public void scan() {
-        List<Vacancy> vacancies = new ArrayList<>();
-        for (Provider provider : providers) {
-            vacancies.addAll(provider.getJavaVacancies("MyTown"));
+    public Controller(Model model) {
+        if (model == null) {
+            throw new IllegalArgumentException();
         }
-        System.out.println(vacancies.size());
+        this.model = model;
+    }
+
+    public void onCitySelect(String cityName) {
+        model.selectCity(cityName);
     }
 }
