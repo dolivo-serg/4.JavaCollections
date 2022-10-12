989e630facb6241fc4c2a34df5172a0bbdc62be8
Sehii <dolivo.serg@gmail.com>
2022-10-12 14:10:26 +0300

    task3901 length Of Longest Unique Substring

--- src/com/javarush/task/task39/task3901/Solution.java
+++ src/com/javarush/task/task39/task3901/Solution.java
@@ -3,8 +3,8 @@ package com.javarush.task.task39.task3901;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.HashSet;
+import java.util.Set;
 
 /* 
 Уникальные подстроки
@@ -23,6 +23,25 @@ public class Solution {
         if (s == null || s.isEmpty()) {
             return 0;
         }
+
+        int maxLength = 0;
+        Set<Character> chars = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (chars.size() > maxLength) {
+                maxLength = chars.size();
+            }
+            chars.clear();
+            for (int j = i; j < s.length(); j++) {
+                char c = s.charAt(j);
+                if (!chars.contains(c)) {
+                    chars.add(c);
+                } else {
+                    break;
+                }
+            }
+        }
+        return Math.max(chars.size(), maxLength);
+/*
         String[] words = s.replaceAll("[^a-zA-Z\\s]", "").trim().split("\\b\\s+\\b");
         return Arrays.stream(words)
                 .map(word -> word.chars()
@@ -34,5 +53,12 @@ public class Solution {
                         .collect(Collectors.joining()))
                 .max(Comparator.comparing(String::length))
                 .get().length();
+*/
+
+/*
+        Map<String, Long> words = Arrays.stream(s.split("\\b\\s+\\b"))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        return words.keySet().stream().mapToInt(String::length).max().getAsInt();
+*/
     }
 }
