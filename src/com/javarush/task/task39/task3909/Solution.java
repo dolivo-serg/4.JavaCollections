76fa3f980d5255ba6e77e4199e2fba64100c26ab
Sehii <dolivo.serg@gmail.com>
2022-10-19 13:20:03 +0300

    task3909  LevenshteinDistance

--- src/com/javarush/task/task39/task3909/Solution.java
+++ src/com/javarush/task/task39/task3909/Solution.java
@@ -6,10 +6,61 @@ package com.javarush.task.task39.task3909;
 
 public class Solution {
     public static void main(String[] args) {
-
+        System.out.println(isOneEditAway("ne", "one"));
     }
+/*
+    //import org.apache.commons.text.similarity.LevenshteinDistance;
+
+        public static boolean isOneEditAway(String first, String second) {
+            int differences = LevenshteinDistance.getDefaultInstance().apply(first, second);
+            return  differences < 2;
+        }
+*/
 
     public static boolean isOneEditAway(String first, String second) {
+        int lengthDifference = first.length() - second.length();
 
+        if (lengthDifference == 0) {
+            return checkReplacement(first, second);
+        } else if (lengthDifference == 1) {
+            return checkInsertion(first, second);
+        } else if (lengthDifference == -1) {
+            return checkInsertion(second, first);
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean checkReplacement(String first, String second) {
+        boolean foundDifference = false;
+        for (int i = 0; i < first.length(); i++) {
+            if (first.charAt(i) != second.charAt(i)) {
+                if (foundDifference) {
+                    return false;
+                } else {
+                    foundDifference = true;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkInsertion(String first, String second) {
+        int i = 0;
+        int j = 0;
+
+        while (j < second.length() && i < first.length()) {
+            if (first.charAt(i) != second.charAt(j)) {
+                if (i != j) {
+                    return false;
+                }
+                i++;
+            } else {
+                i++;
+                j++;
+            }
+        }
+        return true;
     }
 }
+
