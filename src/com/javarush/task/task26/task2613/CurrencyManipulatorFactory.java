c13cb126e715f7107b919cd4684f83159c576c51
Sehii <dolivo.serg@gmail.com>
2022-10-27 18:47:22 +0300

    task2613 CashMachine

---
+++ src/com/javarush/task/task26/task2613/CurrencyManipulatorFactory.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task26.task2613;
+
+public class CurrencyManipulatorFactory {
+
+    public static CurrencyManipulator getManipulatorByCurrencyCode(String currencyCode) {
+
+        return
+    }
+}
