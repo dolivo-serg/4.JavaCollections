4ec289aeecd0e4cc7b37af45603c23eed6f75ca5
Sehii <dolivo.serg@gmail.com>
2022-10-28 20:28:45 +0300

    task2613 CashMachine

--- src/com/javarush/task/task26/task2613/CurrencyManipulatorFactory.java
+++ src/com/javarush/task/task26/task2613/CurrencyManipulatorFactory.java
@@ -1,9 +1,19 @@
 package com.javarush.task.task26.task2613;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CurrencyManipulatorFactory {
+    private static Map<String, CurrencyManipulator> map = new HashMap<>();
+
+    private CurrencyManipulatorFactory() {
+    }
 
     public static CurrencyManipulator getManipulatorByCurrencyCode(String currencyCode) {
-
-        return
+        map.entrySet().stream()
+                .filter(entry -> !entry.getKey().equals(currencyCode.toUpperCase()))
+                .findFirst()
+                .ifPresent(entry -> map.put(currencyCode, new CurrencyManipulator(currencyCode)));
+        return map.get(currencyCode);
     }
 }
--- src/com/javarush/task/task26/task2613/Operation.java
+++ src/com/javarush/task/task26/task2613/Operation.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task26.task2613;
 
 public enum Operation {
-    INFO, DEPOSIT, WITHDRAW, EXIT
+    INFO,
+    DEPOSIT,
+    WITHDRAW,
+    EXIT
 }
