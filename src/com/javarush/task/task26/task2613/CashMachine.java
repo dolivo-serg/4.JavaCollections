a584bffce8674fd1f8f44706d9b07eb56ff7148f
Sehii <dolivo.serg@gmail.com>
2022-10-31 12:06:48 +0200

    task2613 CashMachine

--- src/com/javarush/task/task26/task2613/CashMachine.java
+++ src/com/javarush/task/task26/task2613/CashMachine.java
@@ -12,9 +12,8 @@ public class CashMachine {
         int denomination = Integer.parseInt(digits[0]);
         int count = Integer.parseInt(digits[1]);
 
-        CurrencyManipulator manipulatorByCurrencyCode = CurrencyManipulatorFactory
-                .getManipulatorByCurrencyCode(currencyCode);
-
+        CurrencyManipulator manipulatorByCurrencyCode = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
         manipulatorByCurrencyCode.addAmount(denomination, count);
+        manipulatorByCurrencyCode.getTotalAmount();
     }
 }
