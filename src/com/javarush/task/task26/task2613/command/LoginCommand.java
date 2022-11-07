ebe5d37c4d647d4ac01a17609b561bb232e61cf9
Sehii <dolivo.serg@gmail.com>
2022-11-07 19:52:25 +0200

    task2613 Pattern Command

--- src/com/javarush/task/task26/task2613/command/LoginCommand.java
+++ src/com/javarush/task/task26/task2613/command/LoginCommand.java
@@ -1,12 +1,14 @@
 package com.javarush.task.task26.task2613.command;
 
 
+import com.javarush.task.task26.task2613.CashMachine;
 import com.javarush.task.task26.task2613.ConsoleHelper;
 import com.javarush.task.task26.task2613.exception.InterruptOperationException;
 
+import java.util.ResourceBundle;
+
 class LoginCommand implements Command {
-    private String validCreditCard = "123456789012";
-    private String validPin = "1234";
+    private ResourceBundle validCreditCards = ResourceBundle.getBundle(CashMachine.class.getPackage().getName() + ".resources.verifiedCards");
 
     @Override
     public void execute() throws InterruptOperationException {
@@ -16,14 +18,12 @@ class LoginCommand implements Command {
             ConsoleHelper.writeMessage("Please specify your credit card number and pin code or type 'EXIT' for exiting.");
             String creditCardNumber = ConsoleHelper.readString();
             String pinStr = ConsoleHelper.readString();
-            if (validPin.equalsIgnoreCase("exit")) {
-                throw new InterruptOperationException();
-            } else if (creditCardNumber == null || (creditCardNumber = creditCardNumber.trim()).length() != 12 ||
+            if (creditCardNumber == null || (creditCardNumber = creditCardNumber.trim()).length() != 12 ||
                     pinStr == null || (pinStr = pinStr.trim()).length() != 4) {
                 ConsoleHelper.writeMessage("Please specify valid credit card number - 12 digits, pin code - 4 digits.");
             } else {
                 try {
-                    if (creditCardNumber.equals(validCreditCard) && pinStr.equals(validPin)) {
+                    if (validCreditCards.containsKey(creditCardNumber) && pinStr.equals(validCreditCards.getString(creditCardNumber))) {
                         ConsoleHelper.writeMessage(String.format("Credit card [%s] is verified successfully!", creditCardNumber));
                         break;
                     } else {
